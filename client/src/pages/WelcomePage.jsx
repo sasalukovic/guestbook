881e4c2e545f2442f23539df81c95e572d542892
sasalukovic <sasalukovic1@gmail.com>
2023-05-18 16:39:40 +0200

    Fetch and show data from database

--- client/src/pages/WelcomePage.jsx
+++ client/src/pages/WelcomePage.jsx
@@ -1,5 +1,36 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 const WelcomePage = () => {
-  return <div></div>;
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const res = await axios.get("http://localhost:8800/guestbook");
+        setData(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    getData();
+  }, []);
+
+  return (
+    <div>
+      <h1>Guestbook</h1>
+      <p>See what people wrote about us and feel free to leave a message</p>
+      <div>
+        {data.map((d) => (
+          <div key={d.id}>
+            <h2>{d.name}</h2>
+            <p>{d.message}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default WelcomePage;
