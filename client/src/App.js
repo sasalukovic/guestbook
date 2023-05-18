a159474faf392b1c93c3015143ca2df49f03808c
sasalukovic <sasalukovic1@gmail.com>
2023-05-18 16:07:44 +0200

    Add routes and create pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,25 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import WelcomePage from "./pages/WelcomePage";
+import Message from "./pages/Message";
 import "./App.css";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <WelcomePage />,
+  },
+  {
+    path: "/message",
+    element: <Message />,
+  },
+]);
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
