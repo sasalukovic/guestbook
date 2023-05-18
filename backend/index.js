42acc6896a3d1f652e1d37bafe5cbf82850c81b8
sasalukovic <sasalukovic1@gmail.com>
2023-05-18 15:49:46 +0200

    Connect to database

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,21 @@
 import express from "express";
 
+// using mysql2 because of auth problem with DB
+import mysql2 from "mysql2";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 const app = express();
+
+app.use(express.json());
+
+const db = mysql2.createConnection({
+  host: "localhost",
+  user: "root",
+  password: process.env.DB_PASSWORD,
+  database: "test",
+});
 
 app.listen(8800, () => {
   console.log("Connected to BE");
